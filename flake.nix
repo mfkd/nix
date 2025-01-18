da7d8f1335168166cdadf0892c9636a957a91fb2
mfkd <mattkfd@gmail.com>
2025-01-18 10:32:45 +0100

    nit: remove redundant comment

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 {
   description = "NixOS System Configuration";
   inputs = {
-    # If you want to use the latest upstream version, I recommend using
-    # branch "nixos-unstable" instead of "23.11" (the latest stable release).
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
   outputs = { self, nixpkgs }: {
